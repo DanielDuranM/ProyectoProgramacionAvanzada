b75da7cd0fa306aea30e06d2bbb92205280c7396
DanielDuranM <dduran80149@ufide.ac.cr>
2026-04-26 20:20:46 -0600

    Commit 2

--- ProyectoFinal_DuranDaniel/obj/Release/net8.0/ProyectoFinal_DuranDaniel.AssemblyInfo.cs
+++ ProyectoFinal_DuranDaniel/obj/Release/net8.0/ProyectoFinal_DuranDaniel.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal_DuranDaniel")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c138795dbd5ba38cc8ecf1387647ce35a1d1a9e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal_DuranDaniel")]
